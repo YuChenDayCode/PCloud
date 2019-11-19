a3242e8f818a99fd48e8ceb04005d82dea49f515
YuChen <yuchen_cz@163.com>
2019-11-19 23:03:02 +0800

    file

--- PCloud.Web/wwwroot/js/site.js
+++ PCloud.Web/wwwroot/js/site.js
@@ -5,23 +5,35 @@
 
 
 $(function () {
-    $('.inputfile').each((index, input) => {
-        console.log(input);
-        var $label = $(input).next('label'),
-            labelVal = $label.html();
-        $(input).on('change', function (e) {
-            var fileName = "";
-            console.log(this.files);
-            $(input).blur();
-            fileName = e.target.value.split('\\').pop();
-            if (fileName) {
-                $label.find('span').html(fileName);
-            }
-            else {
-                $label.html(labelVal);
-            }
-        });
-        $(input).on('focus', function () { $(input).addClass('has-focus'); });
-        $(input).on('blur', function () { $(input).removeClass('has-focus'); });
+    var $input = $('#file-upload');
+    var $label = $input.next('label'),
+        labelVal = $label.html();
+    $input.on('change', function (e) {
+        $(this).blur();
+        var fileName = e.target.value.split('\\').pop();
+        if (fileName) $label.find('span').html(fileName);
+        else $label.html(labelVal);
+        console.log(this.files);
+        var tt = 0;
+        $label.css({ "background-color": "#FFB800" });
+        $('.pr-container').css({ "background-color": "white" });
+        var it = setInterval(() => {
+            tt += 2;
+            if (tt > 100) clearInterval(it);
+            progress(tt, $label);
+        }, 100);
     });
 });
+
+
+function progress(t, $label) {
+    if (t <= 100) {
+        var percent = t * 100 / 100;
+        $('#progress').css({ "width": percent.toFixed(2) + '%' });
+    }
+    else {
+        //$('.pr-container').css({ "background-color": "" });
+        //$('#progress').css({ "width": "" });
+        $label.css({ "background-color": "#64c896" });
+    }
+}
